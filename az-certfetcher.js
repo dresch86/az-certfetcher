7e50486da4774363a164512a70c8572156c3ad93
primesoftware <primesoftwarenetworks@gmail.com>
2019-10-09 22:09:15 -0400

    1) Remove unnecessary openssl-nodejs in favor of custom solution; 2) Use promises to handle file work

--- az-certfetcher.js
+++ az-certfetcher.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs-extra';
+import spawn from 'await-spawn';
 
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretsClient } from "@azure/keyvault-secrets";
@@ -8,16 +9,53 @@ const secretName = process.env.SECRET_NAME;
 const vaultName = process.env.KEYVAULT_NAME;
 const pthExecHome = path.resolve(__dirname);
 
-var installPath = process.env.INSTALL_PATH.trim();
-var installName = process.env.INSTALL_NAME.trim();
+const installPath = process.env.INSTALL_PATH.trim();
+const installName = process.env.INSTALL_NAME.trim();
+const sAzurePFX = installPath + path.sep + 'azure.' + installName + '.pfx';
 
-const openssl = require('openssl-nodejs');
+async function convertPFXtoPEM() {
+    try {
+        let sPrivateKeyPath = installPath + path.sep + installName + '.privkey.pem';
+        let sCertificatePath = installPath + path.sep + installName + '.cert.crt';
+    
+        let blPrivKeyRes = await spawn('openssl', ['pkcs12', '-in', sAzurePFX, '-nocerts', '-out', sPrivateKeyPath, '-nodes', '-passin', 'pass:']);
+        let blCertificateRes = await spawn('openssl', ['pkcs12', '-in', sAzurePFX, '-nocerts', '-out', sCertificatePath, '-nodes', '-passin', 'pass:']);
+    
+        if (!(blPrivKeyRes instanceof Error)) {
+            fs.chmod(sPrivateKeyPath, 0o0600, (err) => {
+                if (err) {
+                    console.error('SECURITY: Failed to change permissions on private key!');
+                    return;
+                }
+        
+                console.log('SUCCESS: The permissions for file ' + sPrivateKeyPath + ' have been changed!');
+            });
+        } else {
+            console.log(blPrivKeyRes.stderr.toString('utf8'));
+        }
+    
+        if (!(blCertificateRes instanceof Error)) {
+            fs.chmod(sCertificatePath, 0o0644, (err) => {
+                if (err) {
+                    console.error('SECURITY: Failed to change permissions on certificate file!');
+                    return;
+                }
+        
+                console.log('SUCCESS: The permissions for file ' + sCertificatePath + ' have been changed!');
+            });
+        } else {
+            console.log(blCertificateRes.stderr.toString('utf8'));
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function main() {
     if (installPath.length > 0) {
         try {
             await fs.ensureDir(installPath, {mode: 0o0755});
-            console.log('Directory [' + installPath + '] created!');
+            console.log('Directory [' + installPath + '] present!');
         } catch (err) {
             console.error(err);
             return;
@@ -33,33 +71,13 @@ async function main() {
     const credential = new DefaultAzureCredential();
     const url = `https://${vaultName}.vault.azure.net`;
     const client = new SecretsClient(url, credential);
-
     const latestSecret = await client.getSecret(secretName);
-    const bCertBuff = Buffer.from(latestSecret.value, 'base64');
 
-    let sPrivateKeyPath = installPath + path.sep + installName + '.privkey.pem';
-    let sCertificatePath = installPath + path.sep + installName + '.cert.crt';
-
-    openssl(['pkcs12', '-in', {name: 'bundle.pfx', buffer: bCertBuff}, '-nocerts', '-out', sPrivateKeyPath, '-nodes', '-passin', 'pass:']);
-    openssl(['pkcs12', '-in', {name: 'bundle.pfx', buffer: bCertBuff}, '-nokeys', '-out', sCertificatePath, '-passin', 'pass:']);
-
-    fs.chmod(sPrivateKeyPath, 0o0600, (err) => {
-        if (err) {
-            console.error('SECURITY: Failed to change permissions on private key!');
-            return;
-        }
-
-        console.log('SUCCESS: The permissions for file ' + sPrivateKeyPath + ' have been changed!');
-    });
-
-    fs.chmod(sCertificatePath, 0o0644, (err) => {
-        if (err) {
-            console.error('SECURITY: Failed to change permissions on certificate file!');
-            return;
-        }
-
-        console.log('SUCCESS: The permissions for file ' + sCertificatePath + ' have been changed!');
-    });
+    let writeStream = fs.createWriteStream(sAzurePFX);
+    writeStream.on('error', err => console.error(err));
+    writeStream.on('finish', convertPFXtoPEM);
+    writeStream.write(latestSecret.value, 'base64');
+    writeStream.end();
 }
   
 main().catch(e => console.error(e.stack));
